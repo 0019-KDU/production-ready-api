6db7da24c21e15d367471f75113725f4ab077b1e
CR Hapuarachchi <chirantharavishka@gmail.com>
2025-01-01 16:15:34 +0530

    Middlewares

--- src/index.js
+++ src/index.js
@@ -8,15 +8,25 @@ import movies from "./controllers/movieController.js";
 dotenv.config();
 
 import connect from "./config/db.js";
+import { removeResHeaders } from "./middlewares/removeResHeaders.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(removeResHeaders);
 
-app.get("/ping", (req, res) => {
-  res.status(200).json({ message: "ping" });
-});
+app.get(
+  "/ping",
+  (req, res, next) => {
+    console.log("inside middleware beofore route handler");
+    next();
+  },
+  (req, res) => {
+    console.log("inside ping method route handler");
+    res.status(200).json({ message: "ping" });
+  }
+);
 app.use("/participants", participants);
 app.use("/movies", movies);
 
---
+++ src/middlewares/removeResHeaders.js
@@ -0,0 +1,4 @@
+export const removeResHeaders = (req, res, next) => {
+  res.removeHeader("X-Powered-By");
+  next();
+};
