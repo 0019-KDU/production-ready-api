fe1a66dd3718be71b0eadfa2cca3c07f71028632
CR Hapuarachchi <chirantharavishka@gmail.com>
2025-01-03 20:46:42 +0530

    Role Based Access (Authorization)

--- src/controllers/participantController.js
+++ src/controllers/participantController.js
@@ -8,13 +8,24 @@ import {
 } from "../services/participantsService.js";
 
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { checkPermission } from "../middlewares/roleMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", getParticipants);
 router.get("/:id", getParticipantById);
-router.post("/", authMiddleware, createParticipant);
-router.put("/:id", authMiddleware, updateParticipant);
-router.delete("/:id", authMiddleware, deleteParticipant);
+router.post("/", authMiddleware, checkPermission("create"), createParticipant);
+router.put(
+  "/:id",
+  authMiddleware,
+  checkPermission("update"),
+  updateParticipant
+);
+router.delete(
+  "/:id",
+  authMiddleware,
+  checkPermission("delete"),
+  deleteParticipant
+);
 
 export default router;
